04c920d26f7bfcdaaed4d18a0657bfd8339a2cf8
Sergei Kovalev <summerjob@ngs.ru>
2023-02-07 16:49:09 +0300

    Initial commit

---
+++ src/test/java/ru/ngs/summerjob/spring_course_mvc_hibernate_aop/SpringCourseMvcHibernateAopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ngs.summerjob.spring_course_mvc_hibernate_aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourseMvcHibernateAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
