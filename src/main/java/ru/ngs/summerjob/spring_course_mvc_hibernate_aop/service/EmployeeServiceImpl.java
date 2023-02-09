b1b2dc6c46d7310c51c8ab169552fa087d33bf94
Sergei Kovalev <summerjob@ngs.ru>
2023-02-09 18:16:57 +0300

    lost class added.

---
+++ src/main/java/ru/ngs/summerjob/spring_course_mvc_hibernate_aop/service/EmployeeServiceImpl.java
@@ -0,0 +1,40 @@
+package ru.ngs.summerjob.spring_course_mvc_hibernate_aop.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.ngs.summerjob.spring_course_mvc_hibernate_aop.dao.EmployeeDAO;
+import ru.ngs.summerjob.spring_course_mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeDAO employeeDAO;
+
+    @Override
+    @Transactional
+    public List<Employee> getAllEmployees() {
+        return employeeDAO.getAllEmployees();
+    }
+
+    @Override
+    @Transactional
+    public void saveEmployee(Employee employee) {
+        employeeDAO.saveEmployee(employee);
+    }
+
+    @Override
+    @Transactional
+    public Employee getEmployeeById(int id) {
+        return employeeDAO.getEmployeeById(id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteEmployee(int id) {
+        employeeDAO.deleteEmployee(id);
+    }
+}
